2d69de9aa9dc74d7eff5d67e55ca8d430b25ec60
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-07-31 22:04:37 -0300

    Create Json

--- Exemplo_copilot.java
+++ Exemplo_copilot.java
@@ -1,13 +1,9 @@
 public class Exemplo_copilot {
-    // Crie uma constante chamada "produtos" contendo uma lista de objetos json.
+    // Crie uma constante chamada "produtos" contendo uma lista de objetos Json.
     // Cada objeto deve representar um produto com as seguintes propriedades:
     //id, nome,preco, descrição e categoria.
-    // A lista deve conter pelo menos 3 produtos diferentes.
-    public static void main(String[] args) {
-        String produtos = "[{\"id\": 1, \"nome\": \"Produto A\", \"preco\": 10.99, \"descricao\": \"Descrição do Produto A\", \"categoria\": \"Categoria 1\"},"
-                + "{\"id\": 2, \"nome\": \"Produto B\", \"preco\": 20.49, \"descricao\": \"Descrição do Produto B\", \"categoria\": \"Categoria 2\"},"
-                + "{\"id\": 3, \"nome\": \"Produto C\", \"preco\": 15.75, \"descricao\": \"Descrição do Produto C\", \"categoria\": \"Categoria 1\"}]";
-
-        System.out.println(produtos);
-    }
+    // A lista deve conter pelo menos 3 produtos diferentes no formato Json.
+    public static final String produtos = "[{\"id\": 1, \"nome\": \"Produto A\", \"preco\": 29.99, \"descricao\": \"Descrição do Produto A\", \"categoria\": \"Categoria 1\"},"
+            + "{\"id\": 2, \"nome\": \"Produto B\", \"preco\": 49.99, \"descricao\": \"Descrição do Produto B\", \"categoria\": \"Categoria 2\"},"
+            + "{\"id\": 3, \"nome\": \"Produto C\", \"preco\": 19.99, \"descricao\": \"Descrição do Produto C\", \"categoria\": \"Categoria 1\"}]";
 }
