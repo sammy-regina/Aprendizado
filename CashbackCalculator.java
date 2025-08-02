63585f0475bef2c2ee76a6a2ab5cdf3f1c452662
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-08-01 22:17:16 -0300

    Create
    arquivo do desafio cashback

---
+++ CashbackCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CashbackCalculator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double productsValue = scan.nextDouble();
+        scan.nextLine();
+        String couponCode = scan.nextLine();
+
+        double discount = 0;
+        double cashback = 0;
+
+        if (couponCode.equals("DESC10")) {
+            discount = productsValue * 0.10;
+        }
+
+        double discountedValue = productsValue - discount;
+
+        if (discountedValue >= 150.00)
+            cashback = 15.0;
+
+        double finalValue = discountedValue - cashback;
+
+        System.out.printf("Valor final: R$ %.2f | Cashback: R$ %.2f\n", finalValue, cashback);
+
+        scan.close();
+    }
+}
