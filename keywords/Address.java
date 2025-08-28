1b2b30b5192a7210415c3798f528537165e80fdf
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-08-28 19:18:43 -0300

    Update
    adicionei mais coisas

---
+++ keywords/Address.java
@@ -0,0 +1,13 @@
+package keywords;
+
+public class Address {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ keywords/Main.java
@@ -0,0 +1,14 @@
+package keywords;
+
+import interfaces_funcionais.User;
+
+public class Main {
+    public static void main(String[] args) {
+        var user = new Client();
+        user.setName("John");
+        System.out.println(user.getName());
+        var address = new Address();
+        address.setDescription("123 Main St");
+        System.out.println(address.getDescription());
+    }
+}
