ab9043b027772d0cf5252b58c84a4fc572d9d71a
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-09-01 18:54:00 -0300

    update

--- heranca_Polimorfismo/Colaborador.java
+++ heranca_Polimorfismo/Colaborador.java
@@ -13,42 +13,52 @@ public class Colaborador {
     private Double salario;
 
     public String getCode() {
+
         return code;
     }
 
     public void setCode(String code) {
+
         this.code = code;
     }
 
     public String getNome() {
+
         return nome;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public String getEndereco() {
+
         return endereco;
     }
 
     public void setEndereco(String endereco) {
+
         this.endereco = endereco;
     }
 
     public String getIdade() {
+
         return idade;
     }
 
     public void setIdade(String idade) {
+
         this.idade = idade;
     }
 
     public Double getSalario() {
+
         return salario;
     }
 
     public void setSalario(Double salario) {
+
         this.salario = salario;
     }
 }
