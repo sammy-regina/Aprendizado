85bec5ef214f9287d5c547f675a3d035c902ef8b
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-10-10 23:28:56 -0300

    Comparando senhas

--- estruturaDeControleValidacao/ComparadorSenhas.java
+++ estruturaDeControleValidacao/ComparadorSenhas.java
@@ -1,4 +1,20 @@
 package estruturaDeControleValidacao;
 
+import java.util.Scanner;
+
 public class ComparadorSenhas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String senhaDigitada = sc.nextLine();
+        String senhaConfirmada = sc.nextLine();
+
+        // TODO: Implemente a estrutura condicional que compara as duas strings:
+        if (senhaDigitada.equals(senhaConfirmada)){
+            System.out.println("Senha valida");
+        }else {
+            System.out.println("Senhas nao conferem");
+        }
+        sc.close();
+    }
 }
