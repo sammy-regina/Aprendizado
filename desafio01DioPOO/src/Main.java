34c8f617c8940d6e70d7d9b150719254468406fc
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-11-22 22:45:41 -0300

    desafio herança

--- desafio01DioPOO/src/Main.java
+++ desafio01DioPOO/src/Main.java
@@ -1,4 +1,33 @@
 package desafio01DioPOO.src;
 
+import desafio01DioPOO.src.dominio.Curso;
+import desafio01DioPOO.src.dominio.Mentoria;
+
 public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria Java");
+        mentoria1.setData(java.time.LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria de JavaScript");
+        mentoria2.setDescricao("Descrição mentoria JavaScript");
+        mentoria2.setData(java.time.LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
+    }
 }
--- desafio01DioPOO/src/dominio/Curso.java
+++ desafio01DioPOO/src/dominio/Curso.java
@@ -1,4 +1,51 @@
 package desafio01DioPOO.src.dominio;
 
 public class Curso {
+    /* modificadores de acesso:
+    public - acessível de qualquer lugar
+    protected - acessível dentro do mesmo pacote e subclasses
+    default (sem modificador) - acessível apenas dentro do mesmo pacote
+    private - acessível apenas dentro da própria classe
+     */
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    // construtor vazio
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
