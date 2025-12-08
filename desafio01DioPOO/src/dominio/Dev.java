3d95d8c785dc147e2086460e518d2bfc08df4f4b
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-12-07 21:50:47 -0300

    Update Dev.java

--- desafio01DioPOO/src/dominio/Dev.java
+++ desafio01DioPOO/src/dominio/Dev.java
@@ -2,6 +2,7 @@ package desafio01DioPOO.src.dominio;
 
 import java.util.Objects;
 import java.util.Set;
+import java.util.Optional;
 
 public class Dev {
     private String nome;
@@ -9,12 +10,25 @@ public class Dev {
     private java.util.Set<Conteudo> conteudosConcluidos = new java.util.LinkedHashSet<>(); // Mantém a ordem de conclusão
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());// Adiciona todos os conteúdos do bootcamp aos inscritos (addAll é usado para adicionar uma coleção inteira)
+        bootcamp.getDevsInscritos().add(this); // Adiciona o dev à lista de inscritos do bootcamp
     }
 
     public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst(); // Pega o primeiro conteúdo inscrito
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get()); // Adiciona o conteúdo concluído ao conjunto de concluídos
+            this.conteudosInscritos.remove(conteudo.get()); // Remove o conteúdo do conjunto de inscritos
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
     public double calcularTotalXp() {
+        return this.conteudosConcluidos
+                .stream() // uma stream é usada para processar coleções de forma funcional, ela permite operações como map, filter, reduce, etc.
+                .mapToDouble(conteudo -> conteudo.calcularXp())
+                .sum(); // retrna a soma do XP de todos os conteúdos concluídos
     }
 
     // Getters and Setters
